0ab8dedd11ca88044474610d5aaafaece328f86f
WangWang318 <1172350484@qq.com>
2025-05-25 23:44:02 +0800

    BFS and Loss Function

---
+++ Algorithm/BFS/Switch_Seat.cpp
@@ -0,0 +1,40 @@
+// 换位置
+// "ABCD" 每次可以两两交换，最少需要多少次交换达到 "CDAB"
+
+#include<iostream>
+#include<queue>
+#include<unordered_map>
+using namespace std;
+string str1 = "ACBD"; 
+string str2 = "CDAB";
+unordered_map<string,int> d;
+queue<string> q;
+
+int BFS(){
+    q.push(str1);
+    d[str1] = 0;
+    
+    while(q.size()){
+        string s = q.front(); q.pop();
+        int distance = d[s];
+        if(s == str2)   return distance;
+        for(int i = 0; i < 4; i++){
+            for(int j = i + 1; j < 4; j++){
+                swap(s[i], s[j]);
+                if(!d.count(s)){
+                    d[s] = distance + 1;
+                    q.push(s);
+                }
+                swap(s[i], s[j]);
+            }
+        }
+    }
+    return -1;
+}
+
+int main(){
+
+    int ans = BFS();
+    cout<<ans<<endl;
+    return 0;
+}
--- Algorithm/Sort/quick_sort.md
+++ Algorithm/Sort/quick_sort.md
@@ -52,14 +52,14 @@ int a[N];
 int quick_sort(int l, int r){
     if(l >= r)  return a[l];
     int i = l - 1, j = r + 1;
-    int mid = a[(l + r)/2];
+    int mid = a[l]; // (l + r)/2
     while(i < j){
         while(a[++i] < mid);
         while(a[--j] > mid);
         if(i < j)   swap(a[i], a[j]);
     }
-    // if(j == k && a[j] == mid)  return a[j]; 仔细想想为什么不对
-    if(j >= k)  return quick_sort(l, j);
+    // if(j == k )  return a[j]; 仔细想想为什么不对 判断里面加上&& a[j] == mid才对
+    if(j >= k)  return quick_sort(l, j); // 这两行仔细参照一下二分
     else    return quick_sort(j + 1, r);
 }
 
@@ -74,3 +74,10 @@ int main(){
     return 0;
 }
 ```
+## 例子
+5 2 4 3 1   
+如果要找到第四大的数，也就是k = 3   
+第一轮，结束时i = 4, j = 3  
+5 2 4 3 1  
+1 2 4 ***3*** 5  
+如果直接输出a[j],答案会是3.
