3d70316308cab0aa8b6687d8fc95169a9e89c6b6
WangWang318 <1172350484@qq.com>
2025-11-08 14:32:09 +0800

    Attention

--- Algorithm/BFS/Switch_Seat.cpp
+++ Algorithm/BFS/Switch_Seat.cpp
@@ -7,7 +7,7 @@ string str1 = "abcdefx";
 string str2 = "cabfdex";
 queue<string> q;
 unordered_map<string, int> d;
-
+unordered_map<string, string> pr;
 
 int BFS(){
     q.push(str1);
@@ -17,11 +17,13 @@ int BFS(){
         int dis = d[s];
         if(s == str2)   return dis;
         int idx = s.find('x');
+        string p = s;
         for(int i = 0; i < str1.size(); i++){
             swap(s[idx], s[i]);
             if(!d.count(s)){
                 q.push(s);
                 d[s] = dis + 1;
+                pr[s] = p;
             }
             swap(s[idx], s[i]);
         }
@@ -31,7 +33,12 @@ int BFS(){
 
 int main(){
 
-    cout<<BFS();
-
+    cout<<BFS()<<endl;
+    string s = str2;
+    while(pr.count(s)){
+        cout<<s<<endl;
+        s = pr[s];
+    }
+    cout<<s;
     return 0;
 }
